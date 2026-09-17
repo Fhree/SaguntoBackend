8288451a69cc069d1240561c1f4d8bec8e33a70f
Fhree <luisgvillalonga@gmail.com>
2026-09-17 17:23:47 +0200

    New endpoint to list saguntinos

---
+++ Sagunto.Application/Features/Reports/GetSaguntinosQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Sagunto.Application.Interfaces;
+using Wolverine.Http;
+
+namespace Sagunto.Application.Features.Reports
+{
+    public record SaguntinoDto(string Id, string Name, string Surname, string SaguntinoCode);
+
+    public class GetSaguntinosQueryHandler
+    {
+        [WolverineGet("/api/reports/saguntinos")]
+        [Tags("Reports")]
+        [EndpointSummary("Get the list of Saguntinos")]
+        public static async Task<IReadOnlyList<SaguntinoDto>> HandleAsync(ISaguntoDbContext db, CancellationToken ct)
+        {
+            var query = db.Users.AsNoTracking();
+
+            return await query
+                .OrderBy(s => s.Id)
+                .Select(s => new SaguntinoDto(s.FirebaseUid, s.Name, s.Surname, s.SaguntinoCode))
+                .ToListAsync(ct);
+        }
+    }
+}
